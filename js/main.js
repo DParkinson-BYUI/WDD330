b963cbdc9ad9b5306d528ab8a438ca3573269647
DParkinson-BYUI <par19048@byui.edu>
2021-01-09 16:39:35 -0800

    commit exercise 1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
-const links = [
+  const links = [
     {
-      label: "Week1 notes",
-      url: "week1/index.html"
+      label: "Week 01",
+      url: "week1/"
+    },
+    {
+      label: "Week 01 Notes",
+      url: "week1/notes.html"
+    },
+    {
+      label: "Week 02 Application",
+      url: "week1/app.html"
     }
-  ]
+  ];
+  
+  const orderedlist = document.querySelector("ol");
+  
+  for (const item of links) {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.setAttribute("href", item.url);
+    a.textContent = item.label;
+    li.appendChild(a);
+  
+    orderedlist.appendChild(li);
+  }
